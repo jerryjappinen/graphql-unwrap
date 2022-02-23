b0e1da64c7d8c0fb13106033c97dda9a251bf11e
Jerry Jäppinen <eiskis@gmail.com>
2022-02-23 02:08:18 +0200

    Code style

--- src/treatOptions.js
+++ src/treatOptions.js
@@ -1,8 +1,8 @@
 export default (o) => {
   return {
     idKey: (o && o.idKey) ? o.idKey : 'id',
-    queryPath: (o && o.queryPath) ? o.queryPath : null,
+    typeKey: (o && o.typeKey) ? o.typeKey : '__typename',
     itemsPath: (o && o.itemsPath) ? o.itemsPath : null,
-    typeKey: (o && o.typeKey) ? o.typeKey : '__typename'
+    queryPath: (o && o.queryPath) ? o.queryPath : null
   }
 }
